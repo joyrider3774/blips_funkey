af8bd77b4f51a4dd88df0be545f3900bf983a404
Willems Davy <joyrider3774@hotmail.com>
2024-10-22 10:37:14 +0200

    fix gamestate switching bug

--- src/StageSelect.cpp
+++ src/StageSelect.cpp
@@ -96,182 +96,187 @@ void StageSelect()
                 GameState= GSTitleScreen;
             WorldParts.RemoveAll();
         }
-
-        if(Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_X)] || Input->KeyboardHeld[SDLK_x] || Input->KeyboardHeld[SDLK_z]))
-        {
-            if(LevelEditorMode && (SelectedLevel > 0))
-            {
-                sprintf(Tekst,"Are you sure you want to delete this level:\n%s - Level %d\n\nPress (A) to Delete (X) to Cancel",LevelPackName,SelectedLevel);
-                if (AskQuestion(Tekst))
-                {
-                    sprintf(Tekst,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
-                    if(FileExists(Tekst))
+        
+		//check on gamestate to fix bug where B (above here) and A are pressed at same time when comming from gamestate
+		//it would result in clearing the worldparts but still going back to Game Gamestate with no level loaded !
+		if(GameState == GSStageSelect)
+		{
+			if(Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_X)] || Input->KeyboardHeld[SDLK_x] || Input->KeyboardHeld[SDLK_z]))
+			{
+				if(LevelEditorMode && (SelectedLevel > 0))
+				{
+					sprintf(Tekst,"Are you sure you want to delete this level:\n%s - Level %d\n\nPress (A) to Delete (X) to Cancel",LevelPackName,SelectedLevel);
+					if (AskQuestion(Tekst))
 					{
-						remove(Tekst);
-						sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
-						//only swap levels if we did not edit a level that also exists as a default level
-						if(!FileExists(FileName))
+						sprintf(Tekst,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
+						if(FileExists(Tekst))
 						{
-							for(Teller=SelectedLevel;Teller<InstalledLevels;Teller++)
+							remove(Tekst);
+							sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
+							//only swap levels if we did not edit a level that also exists as a default level
+							if(!FileExists(FileName))
 							{
-								sprintf(Tekst,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, Teller+1);
-								sprintf(Tekst1,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName,Teller);                      
-								rename(Tekst,Tekst1);
+								for(Teller=SelectedLevel;Teller<InstalledLevels;Teller++)
+								{
+									sprintf(Tekst,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, Teller+1);
+									sprintf(Tekst1,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName,Teller);                      
+									rename(Tekst,Tekst1);
+								}
+							}
+							FindLevels();
+							if (SelectedLevel > InstalledLevels)
+								SelectedLevel = InstalledLevels;
+							if (SelectedLevel==0)
+								WorldParts.RemoveAll();
+							else
+							{
+								sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev",getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName,SelectedLevel);
+								if(!FileExists(FileName))
+									sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
+								WorldParts.Load(FileName);
 							}
 						}
-						FindLevels();
-						if (SelectedLevel > InstalledLevels)
-							SelectedLevel = InstalledLevels;
-						if (SelectedLevel==0)
-							WorldParts.RemoveAll();
-						else
+					}
+					Input->Reset();
+				}
+				Input->Delay();
+			}
+
+			if(Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_A)] || Input->KeyboardHeld[SDLK_a] || Input->KeyboardHeld[SDLK_q] || Input->KeyboardHeld[SDLK_s] || Input->KeyboardHeld[SDLK_SPACE] || Input->KeyboardHeld[SDLK_RETURN]))
+			{
+				if (GlobalSoundEnabled)
+					Mix_PlayChannel(-1,Sounds[SND_SELECT],0);
+				if(LevelEditorMode)
+				{
+					LevelHasChanged = false;
+					GameState = GSLevelEditor;
+				}
+				else
+					if (SelectedLevel <= UnlockedLevels)
+						GameState = GSGame;
+					else
+					{
+						sprintf(Tekst,"This Level Hasn't been unlocked yet!\nDo you want to play the last unlocked\nlevel %d/%d\n\nPress (A) to Play (X) to Cancel",UnlockedLevels,InstalledLevels);
+						if	(AskQuestion(Tekst))
 						{
-							sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev",getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName,SelectedLevel);
+							SelectedLevel = UnlockedLevels;
+							sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
 							if(!FileExists(FileName))
-								sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
+								sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName, SelectedLevel);
 							WorldParts.Load(FileName);
+							GameState = GSGame;
 						}
-                    }
-                }
-                Input->Reset();
-            }
-            Input->Delay();
-        }
+						Input->Reset();
+					}
+				Input->Delay();
+			}
 
-        if(Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_A)] || Input->KeyboardHeld[SDLK_a] || Input->KeyboardHeld[SDLK_q] || Input->KeyboardHeld[SDLK_s] || Input->KeyboardHeld[SDLK_SPACE] || Input->KeyboardHeld[SDLK_RETURN]))
-        {
-            if (GlobalSoundEnabled)
-                Mix_PlayChannel(-1,Sounds[SND_SELECT],0);
-            if(LevelEditorMode)
-            {
-                LevelHasChanged = false;
-                GameState = GSLevelEditor;
-            }
-            else
-                if (SelectedLevel <= UnlockedLevels)
-                    GameState = GSGame;
-                else
-                {
-                    sprintf(Tekst,"This Level Hasn't been unlocked yet!\nDo you want to play the last unlocked\nlevel %d/%d\n\nPress (A) to Play (X) to Cancel",UnlockedLevels,InstalledLevels);
-                    if	(AskQuestion(Tekst))
-                    {
-						SelectedLevel = UnlockedLevels;
- 						sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
-                        if(!FileExists(FileName))
-							sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName, SelectedLevel);
-                        WorldParts.Load(FileName);
-                        GameState = GSGame;
-                    }
-                    Input->Reset();
-                }
-            Input->Delay();
-        }
-
-        if (Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_L)] || Input->KeyboardHeld[SDLK_PAGEDOWN] || Input->KeyboardHeld[SDLK_m]))
-        {
-            if(LevelEditorMode)
-            {
-				SelectedLevel -= 5;
-                if (SelectedLevel <= 0)
-                {
-                    SelectedLevel = 0;
-                    WorldParts.RemoveAll();
-                }
-                else
-                {
-                    sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
-                    if(!FileExists(FileName))
-						sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
-                        
-                    WorldParts.Load(FileName);
-                }
-            }
-            else
-            {
-				if (SelectedLevel != 1)
+			if (Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_L)] || Input->KeyboardHeld[SDLK_PAGEDOWN] || Input->KeyboardHeld[SDLK_m]))
+			{
+				if(LevelEditorMode)
 				{
 					SelectedLevel -= 5;
-                	if (SelectedLevel < 1)
-                    	SelectedLevel = 1;
-                	sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
-                    if(!FileExists(FileName))
-						sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
-                        
-                	WorldParts.Load(FileName);
+					if (SelectedLevel <= 0)
+					{
+						SelectedLevel = 0;
+						WorldParts.RemoveAll();
+					}
+					else
+					{
+						sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
+						if(!FileExists(FileName))
+							sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
+							
+						WorldParts.Load(FileName);
+					}
 				}
-            }
-            Input->Delay();
-        }
-
-        if (Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_R)] || Input->KeyboardHeld[SDLK_PAGEUP] || Input->KeyboardHeld[SDLK_n]))
-        {           
-			if (SelectedLevel != InstalledLevels)
-			{
-				SelectedLevel +=5;
-            	if (SelectedLevel > InstalledLevels)
-                	SelectedLevel = InstalledLevels;
-            	sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
-                if(!FileExists(FileName))
-					sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
-                        
-            	WorldParts.Load(FileName);
-			}
-            Input->Delay();
-        }
-
-        if (Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_LEFT)] || Input->KeyboardHeld[SDLK_l] || Input->KeyboardHeld[SDLK_LEFT] ))
-        {
-            
-            if(LevelEditorMode)
-            {	
-				SelectedLevel--;
-                if (SelectedLevel <= 0)
-                {
-                    SelectedLevel = 0;
-                    WorldParts.RemoveAll();
-                }
-                else
-                {
-                    sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
-                    if(!FileExists(FileName))
-						sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
-                        
-                    WorldParts.Load(FileName);
-                }
-            }
-            else
-            {
-				if(SelectedLevel != 1)
+				else
 				{
-					SelectedLevel--;
-                	if (SelectedLevel < 1)
-                    	SelectedLevel = 1;
-                	sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
-                    if(!FileExists(FileName))
-						sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);	
-                	WorldParts.Load(FileName);
+					if (SelectedLevel != 1)
+					{
+						SelectedLevel -= 5;
+						if (SelectedLevel < 1)
+							SelectedLevel = 1;
+						sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
+						if(!FileExists(FileName))
+							sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
+							
+						WorldParts.Load(FileName);
+					}
 				}
-            }
-
-            Input->Delay();
-        }
-
-        if (Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_RIGHT)] || Input->KeyboardHeld[SDLK_r] || Input->KeyboardHeld[SDLK_RIGHT] ))
-        {
-            if(SelectedLevel != InstalledLevels)
-			{
-				SelectedLevel++;
-
-            	if (SelectedLevel > InstalledLevels)
-                	SelectedLevel = InstalledLevels;
-            	sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
-                if(!FileExists(FileName))
-					sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
-                        
-            	WorldParts.Load(FileName);
+				Input->Delay();
 			}
-            Input->Delay();
-        }
+
+			if (Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_R)] || Input->KeyboardHeld[SDLK_PAGEUP] || Input->KeyboardHeld[SDLK_n]))
+			{           
+				if (SelectedLevel != InstalledLevels)
+				{
+					SelectedLevel +=5;
+					if (SelectedLevel > InstalledLevels)
+						SelectedLevel = InstalledLevels;
+					sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
+					if(!FileExists(FileName))
+						sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
+							
+					WorldParts.Load(FileName);
+				}
+				Input->Delay();
+			}
+
+			if (Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_LEFT)] || Input->KeyboardHeld[SDLK_l] || Input->KeyboardHeld[SDLK_LEFT] ))
+			{
+				
+				if(LevelEditorMode)
+				{	
+					SelectedLevel--;
+					if (SelectedLevel <= 0)
+					{
+						SelectedLevel = 0;
+						WorldParts.RemoveAll();
+					}
+					else
+					{
+						sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
+						if(!FileExists(FileName))
+							sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
+							
+						WorldParts.Load(FileName);
+					}
+				}
+				else
+				{
+					if(SelectedLevel != 1)
+					{
+						SelectedLevel--;
+						if (SelectedLevel < 1)
+							SelectedLevel = 1;
+						sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
+						if(!FileExists(FileName))
+							sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);	
+						WorldParts.Load(FileName);
+					}
+				}
+
+				Input->Delay();
+			}
+
+			if (Input->Ready() && (Input->JoystickHeld[0][JoystickSetup->GetButtonValue(BUT_RIGHT)] || Input->KeyboardHeld[SDLK_r] || Input->KeyboardHeld[SDLK_RIGHT] ))
+			{
+				if(SelectedLevel != InstalledLevels)
+				{
+					SelectedLevel++;
+
+					if (SelectedLevel > InstalledLevels)
+						SelectedLevel = InstalledLevels;
+					sprintf(FileName,"%s/.blips_levelpacks/%s/level%d.lev", getenv("HOME") == NULL ? ".": getenv("HOME"), LevelPackFileName, SelectedLevel);
+					if(!FileExists(FileName))
+						sprintf(FileName,"./levelpacks/%s/level%d.lev",LevelPackFileName,SelectedLevel);
+							
+					WorldParts.Load(FileName);
+				}
+				Input->Delay();
+			}
+		}
 
 
         if (alpha < 255)
